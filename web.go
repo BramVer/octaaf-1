363f5b5da4b3fcd23176f914755eb5159875fbbe
Bart Willems <bwillems@protonmail.com>
2018-08-01 13:10:56 +0200

    Added kali qoutes to the web interface
    Signed-off-by: Bart Willems <bwillems@protonmail.com>

--- web.go
+++ web.go
@@ -1,21 +1,37 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"octaaf/models"
 
-var Web *gin.Engine
+	"github.com/fatih/structs"
+	"github.com/gin-gonic/gin"
+	"gopkg.in/telegram-bot-api.v4"
+)
+
+type Web struct{}
 
 func ginit() {
+
+	var web Web
 
 	if OctaafEnv == "production" {
 		gin.SetMode("release")
 	}
 
-	Web = gin.Default()
-	Web.GET("/status", octaafStatus)
-	Web.Run()
+	router := gin.Default()
+	api := router.Group("/api/v1")
+	{
+		api.GET("/health", web.health)
+	}
+
+	kali := api.Group("/kali")
+	{
+		kali.GET("/quote", web.quote)
+	}
+	router.Run()
 }
 
-func octaafStatus(c *gin.Context) {
+func (Web) health(c *gin.Context) {
 	// HTTP status code, either 200 or 500
 	status := 200
 	// User facing status message, contains all services
@@ -47,4 +63,30 @@ func octaafStatus(c *gin.Context) {
 	}
 
 	c.JSON(status, statusMessage)
+}
+
+func (Web) quote(c *gin.Context) {
+	quote := models.Quote{}
+	err := DB.Where("chat_id = ?", KaliID).Order("random()").Limit(1).First(&quote)
+
+	if err != nil {
+		c.JSON(500, err)
+	}
+
+	config := tgbotapi.ChatConfigWithUser{
+		ChatID:             KaliID,
+		SuperGroupUsername: "",
+		UserID:             quote.UserID}
+
+	user, err := Octaaf.GetChatMember(config)
+
+	quoteMap := structs.Map(quote)
+
+	if err != nil {
+		quoteMap["username"] = err
+	} else {
+		quoteMap["username"] = user
+	}
+
+	c.JSON(200, quoteMap)
 }
